93a8ecd6abd8ea7e81b2ccba6f9f131659dc2347
gentleokhai <gentleokhaix@gmail.com>
2026-02-04 15:04:18 +0100

    Redis

--- filmapp/settings.py
+++ filmapp/settings.py
@@ -82,13 +82,20 @@ REST_FRAMEWORK = {
 #     }
 # }
 
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://redis:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+
+
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
     }
 }
 
