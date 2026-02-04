07369a0b2625e4d2b23512746ea7fea2902a4dc1
gentleokhai <gentleokhaix@gmail.com>
2026-02-04 20:05:12 +0100

    clean settings

--- filmapp/settings.py
+++ filmapp/settings.py
@@ -159,16 +159,6 @@ DATABASES = {
 #     }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "films",
-#         "USER": "postgres",
-#         "PASSWORD": "password1234.",
-#         "HOST": "localhost",
-#         "PORT": 5432,
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
